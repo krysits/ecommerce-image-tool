151de18e17326215c7af5cf9730a030fe20ec4d9
Kristaps Ledins <krysits@gmail.com>
2025-10-01 18:09:57 +0300

    Create avif.php
    Added AVIF file support

---
+++ avif.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * This script resizes all JPG images in the input directory to 1000x1000 pixels
+ * and saves them in the output directory with a modified filename.
+ */
+
+include_once ('functions.php');
+
+resizeBatch('*.avif', '_1000x1000.jpg', './input/', './square/', 1000, 1000);
+
+echo "Image resize done.";
