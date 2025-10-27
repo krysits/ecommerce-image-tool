f540bf722eed595c4138cdedeb674d9b69a99d5b
Kristaps Ledins <krysits@gmail.com>
2025-10-27 21:35:25 +0200

    jpeg to jpg fix, avif and gif in landcape format

---
+++ h600jpeg.php
@@ -0,0 +1,13 @@
+<?php
+include_once ('functions.php');
+
+resizeBatch(
+	'*.jpeg',
+	'_1000x600.jpg',
+	$inputDir = './input/',
+	$outputDir = './landscape/',
+    $width = 1000,
+    $height= 600
+);
+
+echo "Image resize done.";
